2cfc6d461416ef94abcdd62cb7f3bd042b00c93a
kamatashoma <75316274+kamatashoma@users.noreply.github.com>
2021-06-28 23:10:54 +0900

    クリップボタンを作成

--- screen/ArticleScreen.js
+++ screen/ArticleScreen.js
@@ -10,7 +10,8 @@ import {
 import { addClip, deleteClip } from '../store/action/user';
 
 import { WebView } from 'react-native-webview';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import ClipButton from '../component/ClipButton';
 
 const styles = StyleSheet.create({
   container: {
@@ -23,23 +24,42 @@ export default ArticleScreen = ({ route }) => {
   const { article } = route.params;
   const dispatch = useDispatch();
 
+  const user = useSelector((state) => state.user);
+  const { clips } = user;
+
+  const isClipped = () => {
+    return clips.some((clip) => clip.url === article.url);
+  };
+
+  const toggleClip = () => {
+    if (isClipped()) {
+      dispatch(deleteClip({ clip: article }));
+    } else {
+      dispatch(addClip({ clip: article }));
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          dispatch(addClip({ clip: article }));
-        }}
-      >
-        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          dispatch(deleteClip({ clip: article }));
-        }}
-      >
-        <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
-      </TouchableOpacity>
+      <ClipButton onPress={toggleClip} enabled={isClipped()} />
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
 };
+
+// {
+//    <TouchableOpacity
+// onPress={() => {
+//   dispatch(addClip({ clip: article }));
+// }}
+// >
+// <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+// </TouchableOpacity>
+// <TouchableOpacity
+// onPress={() => {
+//   dispatch(deleteClip({ clip: article }));
+// }}
+// >
+// <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+// </TouchableOpacity>
+// }
